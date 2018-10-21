af216556f8e8204bc025ddf5ada60fdea666a7cb
Dmitry Suvorov <dsuvorov@gmail.com>
2018-10-21 14:17:57 +0500

    Initial Commit

---
+++ HomeRobot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeRobot
+//
+//  Created by Dmitry Suvorov on 21/10/2018.
+//  Copyright © 2018 homesuvorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
